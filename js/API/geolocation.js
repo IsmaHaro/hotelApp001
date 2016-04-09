83e0e189aa2aa1fb50ec1ea656b1141449ef843e
Isma <ismaharo>
2016-04-09 14:33:54 -0500

    Poniendo rutas

--- js/API/geolocation.js
+++ js/API/geolocation.js
@@ -7,9 +7,13 @@ var geo = {
 	},
 
 	exito: function(position){
-alert("position: "+position.coords.latitude+" "+position.coords.longitude);
 		geo.latitud = position.coords.latitude;
 		geo.longitud = position.coords.longitude;
+
+		var hotel = {
+			lat: 19.046583,
+			lng: -98.207966
+		}
 
 		// OPCIONES DEL MAPA
 		var options = {
@@ -23,6 +27,30 @@ alert("position: "+position.coords.latitude+" "+position.coords.longitude);
 
 		// CREAR EL MAPA
 		var map = new google.maps.Map(document.getElementById("canvas"), options);
+
+		var directionsDisplay = new google.maps.DirectionsRender({
+			map: map
+		});
+
+		var request = {
+			destination: hotel,
+			origin: {
+				lat: geo.latitud,
+				lng: geo.longitud
+			},
+			travelMode: google.maps.TravelMode.DRIVING
+		};
+
+		// CREAR SERVICIO DE DIRECCIONES
+		var directionsService = new google.maps.DirectionsService();
+
+		// SOLICITAR LA RUTA
+		directionsService.route(request, function(response, status){
+			if(status == google.maps.DirectionsStatus.OK){
+				// DESPLEGAR LA RUTA EN EL MAPA
+				directionsDisplay.setDirections(response);
+			}
+		});
 	},
 
 	ponerMapa: function(){
